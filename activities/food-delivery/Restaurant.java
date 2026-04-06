caaa10250eac1cbcfa0b5cdbbfd50e71ba2aa84b
RaghadSaqallah <raghadsaqallah78@gmail.com>
2026-04-06 15:09:19 +0300

    Add files via upload

---
+++ activities/food-delivery/Restaurant.java
@@ -0,0 +1,17 @@
+
+package designpatterns_fooddelivery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Raghad Saqallah
+ * ID:220232444
+ */
+public class Restaurant {
+    String restaurantName; 
+    String restauranLocation;
+    List<MeunItem> menu  = new ArrayList<>();
+    
+}
